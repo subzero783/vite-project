475c4884187219baa72f20ad88479e6edf148dda
Gustavo Amezcua <gusta2897@gmail.com>
2025-04-20 23:20:53 -0700

    feat: left at chapter 3 developing and testing...

---
+++ src/__tests__/posts.test.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+import { describe, expect, test } from '@jest/globals'
+import { createPost } from '../services/posts.js'
+import { Post } from '../db/models/post.js'
+
+describe('creating posts', () => {
+  // Test 1
+  test('with all parameters should succeed', async () => {
+    const post = {
+      title: 'Hello Mongoose!',
+      author: 'Daniel Bugl',
+      contents: 'This post is stored in a MongoDB database using Mongoose.',
+      tags: ['mongoose', 'mongodb'],
+    }
+    const createdPost = await createPost(post)
+    expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+
+    const foundPost = await Post.findById(createdPost._id)
+    expect(foundPost).toEqual(expect.objectContaining(post))
+    expect(foundPost.createdAt).toBeInstanceOf(Date)
+    expect(foundPost.updatedAt).toBeInstanceOf(Date)
+  })
+  // Test 2
+  test('without title should fail', async () => {
+    const post = {
+      author: 'Daniel Bugl',
+      contents: 'Post with no title',
+      tags: ['empty'],
+    }
+    try {
+      await createPost(post)
+    } catch (err) {
+      expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+      expect(err.message).toContain('`title` is required')
+    }
+  })
+  // Test 3
+  test('with minimal parameters should succeed', async () => {
+    const post = {
+      title: 'Only a title',
+    }
+    const createdPost = await createPost(post)
+    expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
